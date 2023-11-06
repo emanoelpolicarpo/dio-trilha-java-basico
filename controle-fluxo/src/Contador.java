5854d7fd0af3ff7f15314f5a6a97dc351bef06b3
emanoelpolicarpo <emanuelpolicarpo@gmail.com>
2023-11-06 14:47:57 -0300

    desafio controle de fluxo

---
+++ controle-fluxo/src/Contador.java
@@ -0,0 +1,15 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.print("Digite o primeiro parâmetro:");
+        int parametroUm = sc.nextInt();
+        System.out.print("Digite o segundo parâmetro:");
+        int parametroDois = sc.nextInt();
+
+        
+    }
+}
